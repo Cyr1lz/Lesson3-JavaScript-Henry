01beb7cb49b7ec9b242c5cbc9da894ecf08ab6ba
Babajide Ibiayo <babajideibiayo@yahoo.com>
2023-09-11 15:04:42 +0100

    modified practice.js file and index.html

--- Fundamentals/Practice.js
+++ Fundamentals/Practice.js
@@ -1,11 +1,143 @@
+// This is a single line comment
 
-const magazine = 'two times three is not four'
-const note = 'two times two is four'
-
-
-
-function checkMagazine(magazine, note) {
-    
+/* This is a 
+    multi-line comment
+*/
+/* 
+Example of a multi-line comment about a function
+Version: 1.0
+@desc - This function process invoices
+and does not take parameter.
+@returns - the total value of theinvoice
+*/
+function processInvoice() {
+    var invoice = new Invoice() 
 }
 
-checkMagazine(magazine, note)
+let beef;
+let myName;
+
+beef ="Bokoto";
+myName = "Babajide"
+const VAT = 7.5; 
+const COUNTRY_CODE = 'NGR';
+
+// Output to developer console using console.log()
+console.log("My name is " + myName + " and I love " + beef + "beef");
+
+// Creates a variable sentence and assign a string to it.
+let sentence = "My name is " + myName + " and I love " + beef + " beef";
+
+// Gets the html paragraph HTML element
+let subheading = document.querySelector('p');
+console.log(subheading) // output element to console
+// Modify the paragraph content programatically
+subheading.textContent="This is an example of how you can change text content.";
+
+subheading.textContent = sentence;
+
+/* 
+Data Types 
+1. number
+2. string
+3. undefined
+4. Boolean
+5. Bigint
+6. Symbol
+7. null
+8. object
+*/
+
+/* 
+Number Data Type 
+Floating point and Integer numbers
+*/
+let myInteger = 7; // This is called an integer number
+let myFloat = 7.5; // This is called a floating 
+
+/* 
+String Data Type 
+Single quote and double quote strings
+*/
+let firstName = "Babajide";
+let lastname = 'Ibiayo';
+
+/* Booleans */
+
+let test = true;
+let error = false;
+
+if (true) {
+    console.log('You passed the test')
+    //-> 'You passed the test
+}
+
+if (false) {
+    console.log('You passed the test')
+} else {
+    console.log('Sorry try again');
+    //-> 'Sorry try again
+}
+
+/* 
+    null Data Type 
+    indicates an empty value
+*/
+let middleName = null;
+console.log(middleName);
+
+
+/* 
+    object data type 
+    object have key/value pairs. keys are object properties. values are the value assigned to properties
+*/
+let table = {
+    width: '72cm', //the width property 
+    height: '180cm', // the height property
+    texture: 'mohagony'// the texture property
+};
+console.log(table);
+// Change table properties
+table.texture = "Marble";
+table.height = "140cm";
+// Output table with modified properties
+console.log(table);
+
+// Example of a car object
+let car = {
+    doors: 4,
+    engine: 'v6'
+}
+
+// Simulating a baby toy
+let babyToy = {
+    eyeColor: 'blue',
+    weight: '1.5kg', // property
+    cry: function() {
+        console.log("YeeeeeeeMummmy!!!!");
+    } //method
+}
+
+babyToy.cry(); // Execute the babyToy object method property
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
