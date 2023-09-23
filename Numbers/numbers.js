1f3306c7f4b4b2f7684fdcef12c17706bd56d192
Babajide Ibiayo <babajideibiayo@yahoo.com>
2023-09-23 08:53:52 +0100

    modified directory structure and lesson files

---
+++ Numbers/numbers.js
@@ -0,0 +1,29 @@
+/* Add two numbers in Javascript */
+let myVar = 4 + 8;
+console.log(myVar); //-> 12
+
+/* Subtract two Numbers with JavaScript */
+myVar = 8 - 4;
+console.log(myVar); //-> 4
+
+/* Multiply two Numbers with JavaScript */
+myVar = 8 * 4;
+console.log(myVar); //-> 32
+
+/* Divide One Number by Another with JavaScript */
+myVar = 8 / 4;
+console.log(myVar); //-> 2
+
+/* Increment a Number with JavaScript */
+let i = 0;
+i++; //-> 1 - Increment Operator
+
+// is the equivalent of
+i = i + 1; //-> 2
+
+/* Decrement a Number with JavaScript */
+i = 2;
+i--; //-> 1 - Decrement Operator
+
+// is the equivalent of
+i = i - 1; //-> 0
